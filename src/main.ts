c8d79db3c68f197ad79f791771fcd1ef9d547fa0
Jin Kai <sangwell@126.com>
2024-12-18 14:48:23 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './assets/main.css'
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 import Antd from 'ant-design-vue';
 
 import App from './App.vue'
@@ -12,6 +12,13 @@ const app = createApp(App)
 // 定义一个全局方法用于格式化日期
 app.config.globalProperties.$formatDate = (value, format = 'YYYY-MM-DD') => {
   if (!value) return '';
+  const today = moment();
+  const diffInDays = today.diff(moment(value), 'days');
+  if (diffInDays === 0) {
+    return '今天';
+  } else if (diffInDays === 1) {
+    return '昨天';
+  }
   return moment(value).format(format);
 };
 
