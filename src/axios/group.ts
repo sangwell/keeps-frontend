d562b20138fc186f4f56f513f84c3d90fd265f6c
Jin Kai <sangwell@126.com>
2024-12-18 13:59:28 +0800

    update

--- src/axios/group.ts
+++ src/axios/group.ts
@@ -4,26 +4,26 @@ import axios from 'axios';
  * 查询分组
  */
 export function getGroups() {
-  return axios.get('http://localhost:8066/groups');
+  return axios.get('http://localhost:8066/groups/get');
 }
 
 /**
  * 新增分组
  */
 export function addGroup(data: any) {
-  return axios.post('http://localhost:8066/group', data);
+  return axios.post('http://localhost:8066/groups/add', data);
 }
 
 /**
  * 修改分组名称
  */
 export function updateGroupName(data: any) {
-  return axios.put('http://localhost:8066/updateGroupName', data);
+  return axios.put('http://localhost:8066/groups/updateGroupName', data);
 }
 
 /**
  * 删除分组
  */
 export function deleteGroup(id: string) {
-  return axios.delete(`http://localhost:8066/group/${id}`);
+  return axios.delete(`http://localhost:8066/groups/delete/${id}`);
 }
--- src/axios/plan.ts
+++ src/axios/plan.ts
@@ -4,33 +4,33 @@ import axios from 'axios';
  * 查询计划
  */
 export function getPlans(searchValue) {
-  return axios.get(`http://localhost:8066/plans?searchValue=${searchValue}`);
+  return axios.get(`http://localhost:8066/plans/get?searchValue=${searchValue}`);
 }
 
 /**
  * 根据groupId查询学习计划
  */
 export function getPlansByGroupId(groupId: string) {
-  return axios.get(`http://localhost:8066/plansByGroupId/${groupId}`);
+  return axios.get(`http://localhost:8066/plans/getByGroupId/${groupId}`);
 }
 
 /**
  * 新增计划
  */
 export function addPlan(data: any) {
-  return axios.post('http://localhost:8066/plan', data);
+  return axios.post('http://localhost:8066/plans/add', data);
 }
 
 /**
  * 修改计划
  */
 export function updatePlan(data: any) {
-  return axios.put('http://localhost:8066/updatePlan', data);
+  return axios.put('http://localhost:8066/plans/update', data);
 }
 
 /**
  * 删除计划
  */
 export function deletePlan(id: string) {
-  return axios.delete(`http://localhost:8066/plan/${id}`);
+  return axios.delete(`http://localhost:8066/plans/delete/${id}`);
 }
